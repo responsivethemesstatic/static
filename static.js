0f49febf756aeb6537ac05e34ee414670f34bd3a
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2019-08-10 01:27:31 +0300

    Update static.js

--- static.js
+++ static.js
@@ -1,1 +1,3 @@
-
+if ($("#adsyed").length) {
+document.getElementById("adsyed").innerHTML = '<center><a href="http://corneey.com/w22j1h" target="_blank"><img src="https://rollercoin.com/static/img/referral/banners/ref_72890.gif" width="728" height="90" border="0" alt="" /></a></center>';  
+    }
