36566f4f0d227bc5711a3c5f7e2ce389766da196
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2019-08-10 01:33:37 +0300

    Update static.js

--- static.js
+++ static.js
@@ -1,3 +1,3 @@
 if ($("#adsyed").length) {
-document.getElementById("adsyed").innerHTML = '<center><iframe src="http://coinmedia.co/new_code_site120523.js?ic" scrolling="no" frameborder="0" width="728px" height="90px"></iframe></center>';  
+document.getElementById("adsyed").innerHTML = '<center><iframe src="https://coinmedia.co/new_code_site120523.js?ic" scrolling="no" frameborder="0" width="728px" height="90px"></iframe></center>';  
     }
