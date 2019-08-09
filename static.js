3ad7269c4988417b82f325534f430aba3b5a555c
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2019-08-10 01:55:53 +0300

    Update static.js

--- static.js
+++ static.js
@@ -1,3 +1,7 @@
-if ($("#adsyed").length) {
-document.getElementById("adsyed").innerHTML = '<center><iframe src="https://coinmedia.co/new_code_site120523.js?ic" scrolling="no" frameborder="0" width="728px" height="90px"></iframe></center>';  
+var loc= window.location.href.toString(); 
+var res = loc.match(/http.*:\/\/(.*).blogspot./);
+if(res[1] === "testertser"){
+    if ($("#adsyed").length) {
+        document.getElementById("adsyed").innerHTML = '<center><iframe src="https://coinmedia.co/new_code_site120523.js?ic" scrolling="no" frameborder="0" width="728px" height="90px"></iframe></center>';  
     }
+}
