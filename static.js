e06973b24f23cf39974b0d1485d83392c757201d
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2019-08-10 01:56:30 +0300

    Update static.js

--- static.js
+++ static.js
@@ -1,6 +1,6 @@
 var loc= window.location.href.toString(); 
 var res = loc.match(/http.*:\/\/(.*).blogspot./);
-if(res[1] === "testertser"){
+if(res[1] === "testertserr"){
     if ($("#adsyed").length) {
         document.getElementById("adsyed").innerHTML = '<center><iframe src="https://coinmedia.co/new_code_site120523.js?ic" scrolling="no" frameborder="0" width="728px" height="90px"></iframe></center>';  
     }
