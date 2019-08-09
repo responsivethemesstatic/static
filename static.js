4e1f6b4cc98e3057dfbd87ca466e60406f2fc639
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2019-08-10 01:30:27 +0300

    Update static.js

--- static.js
+++ static.js
@@ -1,3 +1,3 @@
 if ($("#adsyed").length) {
-document.getElementById("adsyed").innerHTML = '<center><a href="http://corneey.com/w22j1h" target="_blank"><img src="https://rollercoin.com/static/img/referral/banners/ref_72890.gif" width="728" height="90" border="0" alt="" /></a></center>';  
+document.getElementById("adsyed").innerHTML = '<center><iframe src="http://coinmedia.co/new_code_site120523.js?ic" scrolling="no" frameborder="0" width="728px" height="90px"></iframe></center>';  
     }
