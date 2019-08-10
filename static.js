6a4d234594f19bd053cff6ef1e29cf69c6409fbf
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2019-08-10 05:11:14 +0300

    Update static.js

--- static.js
+++ static.js
@@ -1,11 +1,16 @@
-var loc= window.location.href.toString(); 
+aimeralynvar loc= window.location.href.toString(); 
 var res = loc.match(/http.*:\/\/(.*).blogspot./);
+/*
 if(res[1] === "aimeralyn"){
     document.getElementById("adsyed").innerHTML = '<center><iframe src="https://coinmedia.co/new_code_site120525.js?ic" scrolling="no" frameborder="0" width="728px" height="90px"></iframe></center>';
     document.getElementById("adsdort").innerHTML = '<center><iframe src="https://coinmedia.co/new_code_site120526.js?ic" scrolling="no" frameborder="0" width="468px" height="60px"></iframe></center>'; 
     document.getElementById("adsucyuz").innerHTML = '<center><iframe src="https://coinmedia.co/new_code_site120527.js?ic" scrolling="no" frameborder="0" width="300px" height="250px"></iframe></center>';
 }
+*/
+    document.getElementById("adsyed").innerHTML = '<center><a href="https://rebrand.ly/guyebasyy" target="_blank"><img src="https://lh3.googleusercontent.com/_nxGU-LmrfgnyRqu435EOeo2_hGgeY1XfKNwczsAr_3m58k_ZKLPyPNZfzYr3Ypa1_w7Hgm_PQi3cPLjGGFx-RbxJOAibZngzZzNhDU3hbtGINFHpIuPrWpbRUJuSnK3EZ753MQQ_A=w728-h90-no" width="728" height="90" border="0" alt="" /></a></center>';
+    document.getElementById("adsdort").innerHTML = '<center><a href="https://rebrand.ly/guyebasyy" target="_blank"><img src="https://lh3.googleusercontent.com/_nxGU-LmrfgnyRqu435EOeo2_hGgeY1XfKNwczsAr_3m58k_ZKLPyPNZfzYr3Ypa1_w7Hgm_PQi3cPLjGGFx-RbxJOAibZngzZzNhDU3hbtGINFHpIuPrWpbRUJuSnK3EZ753MQQ_A=w728-h90-no" width="728" height="90" border="0" alt="" /></a></center>'; 
+    document.getElementById("adsucyuz").innerHTML = '<center><a href="https://rebrand.ly/guyebasyy" target="_blank"><img src="https://lh3.googleusercontent.com/22ZAePIMur_vKvRXHVG1f_ah5MQuPrFFHLFVZAZGO33Riy0ZLkNyCgyKQLZRC8rOA0MYcUCHxR2aCdwUD4rD8KXojjX46TwFtmqB8E1eSCRtEv4d6XnN5jhqgxcY1ahOxmxz3HJhgA=w300-h250-no" width="300" height="250" border="0" alt="" /></a></center>';
 //static
-if ($("#ads").length) {
+if (document.getElementById("ads").innerHTML.length) {
        document.getElementById("ads").innerHTML = '<center><a href="http://corneey.com/w22j1h" target="_blank"><img src="https://rollercoin.com/static/img/referral/banners/ref_72890.gif" width="728" height="90" border="0" alt="" /></a></center>'; 
 }
