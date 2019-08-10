fb9045b208f722bddaa9794befe7ce78b4410122
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2019-08-10 07:46:07 +0300

    Update static.js

--- static.js
+++ static.js
@@ -2,14 +2,21 @@ var loc= window.location.href.toString();
 var res = loc.match(/http.*:\/\/(.*).blogspot./);
 /*
 if(res[1] === "aimeralyn"){
-    document.getElementById("adsyed").innerHTML = '<center><iframe src="https://coinmedia.co/new_code_site120525.js?ic" scrolling="no" frameborder="0" width="728px" height="90px"></iframe></center>';
-    document.getElementById("adsdort").innerHTML = '<center><iframe src="https://coinmedia.co/new_code_site120526.js?ic" scrolling="no" frameborder="0" width="468px" height="60px"></iframe></center>'; 
-    document.getElementById("adsucyuz").innerHTML = '<center><iframe src="https://coinmedia.co/new_code_site120527.js?ic" scrolling="no" frameborder="0" width="300px" height="250px"></iframe></center>';
+    document.getElementById("adsyed").innerHTML = '<center></center>';
+    document.getElementById("adsdort").innerHTML = '<center></center>'; 
+    document.getElementById("adsucyuz").innerHTML = '<center></center>';
 }
 */
-    document.getElementById("adsyed").innerHTML = '<center><a href="https://rebrand.ly/guyebasyy" target="_blank"><img src="https://lh3.googleusercontent.com/_nxGU-LmrfgnyRqu435EOeo2_hGgeY1XfKNwczsAr_3m58k_ZKLPyPNZfzYr3Ypa1_w7Hgm_PQi3cPLjGGFx-RbxJOAibZngzZzNhDU3hbtGINFHpIuPrWpbRUJuSnK3EZ753MQQ_A=w728-h90-no" width="728" height="90" border="0" alt="" /></a></center>';
-    document.getElementById("adsdort").innerHTML = '<center><a href="https://rebrand.ly/guyebasyy" target="_blank"><img src="https://lh3.googleusercontent.com/_nxGU-LmrfgnyRqu435EOeo2_hGgeY1XfKNwczsAr_3m58k_ZKLPyPNZfzYr3Ypa1_w7Hgm_PQi3cPLjGGFx-RbxJOAibZngzZzNhDU3hbtGINFHpIuPrWpbRUJuSnK3EZ753MQQ_A=w728-h90-no" width="728" height="90" border="0" alt="" /></a></center>'; 
-    document.getElementById("adsucyuz").innerHTML = '<center><a href="https://rebrand.ly/guyebasyy" target="_blank"><img src="https://lh3.googleusercontent.com/22ZAePIMur_vKvRXHVG1f_ah5MQuPrFFHLFVZAZGO33Riy0ZLkNyCgyKQLZRC8rOA0MYcUCHxR2aCdwUD4rD8KXojjX46TwFtmqB8E1eSCRtEv4d6XnN5jhqgxcY1ahOxmxz3HJhgA=w300-h250-no" width="300" height="250" border="0" alt="" /></a></center>';
+document.getElementById("adsyed").innerHTML = '<center><a href="https://rebrand.ly/guyebasyy" target="_blank"><img src="https://lh3.googleusercontent.com/_nxGU-LmrfgnyRqu435EOeo2_hGgeY1XfKNwczsAr_3m58k_ZKLPyPNZfzYr3Ypa1_w7Hgm_PQi3cPLjGGFx-RbxJOAibZngzZzNhDU3hbtGINFHpIuPrWpbRUJuSnK3EZ753MQQ_A=w728-h90-no" width="728" height="90" border="0" alt="" /></a></center>';
+document.getElementById("adsdort").innerHTML = '<center><a href="https://rebrand.ly/guyebasyy" target="_blank"><img src="https://lh3.googleusercontent.com/_nxGU-LmrfgnyRqu435EOeo2_hGgeY1XfKNwczsAr_3m58k_ZKLPyPNZfzYr3Ypa1_w7Hgm_PQi3cPLjGGFx-RbxJOAibZngzZzNhDU3hbtGINFHpIuPrWpbRUJuSnK3EZ753MQQ_A=w728-h90-no" width="728" height="90" border="0" alt="" /></a></center>'; 
+document.getElementById("adsucyuz").innerHTML = '<center><a href="https://rebrand.ly/guyebasyy" target="_blank"><img src="https://lh3.googleusercontent.com/22ZAePIMur_vKvRXHVG1f_ah5MQuPrFFHLFVZAZGO33Riy0ZLkNyCgyKQLZRC8rOA0MYcUCHxR2aCdwUD4rD8KXojjX46TwFtmqB8E1eSCRtEv4d6XnN5jhqgxcY1ahOxmxz3HJhgA=w300-h250-no" width="300" height="250" border="0" alt="" /></a></center>';
+
+if(res[1] === "ayzwa"){
+    document.getElementById("adsyed").innerHTML = '<center><iframe data-aa="1222572" src="//ad.a-ads.com/1222572?size=728x90" scrolling="no" style="width:728px; height:90px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe></center>';
+    document.getElementById("adsdort").innerHTML = '<center><iframe data-aa="1222573" src="//ad.a-ads.com/1222573?size=468x60" scrolling="no" style="width:468px; height:60px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe></center>'; 
+    document.getElementById("adsucyuz").innerHTML = '<center><iframe data-aa="1222574" src="//ad.a-ads.com/1222574?size=300x250" scrolling="no" style="width:300px; height:250px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe></center>';
+}
+ 
 //static
 if (document.getElementById("ads").innerHTML.length) {
        document.getElementById("ads").innerHTML = '<center><a href="http://corneey.com/w22j1h" target="_blank"><img src="https://rollercoin.com/static/img/referral/banners/ref_72890.gif" width="728" height="90" border="0" alt="" /></a></center>'; 
