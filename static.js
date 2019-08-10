02ea31ee8dc11230730bbeb36cd1848d60f05abc
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2019-08-10 03:13:49 +0300

    Update static.js

--- static.js
+++ static.js
@@ -1,10 +1,17 @@
 var loc= window.location.href.toString(); 
 var res = loc.match(/http.*:\/\/(.*).blogspot./);
-if(res[1] === "testertser"){
+if(res[1] === "aimeralyn"){
     if ($("#adsyed").length) {
-        document.getElementById("adsyed").innerHTML = '<center><iframe src="https://coinmedia.co/new_code_site120523.js?ic" scrolling="no" frameborder="0" width="728px" height="90px"></iframe></center>';  
+        document.getElementById("adsyed").innerHTML = '<center><iframe src="https://coinmedia.co/new_code_site120525.js?ic" scrolling="no" frameborder="0" width="728px" height="90px"></iframe></center>';  
+    }
+    if ($("#adsdort").length) {
+       document.getElementById("adsdort").innerHTML = '<center><iframe src="https://coinmedia.co/new_code_site120526.js?ic" scrolling="no" frameborder="0" width="468px" height="60px"></iframe></center>'; 
+    }
+    if ($("#adsucyuz").length) {
+       document.getElementById("adsucyuz").innerHTML = '<center><iframe src="https://coinmedia.co/new_code_site120527.js?ic" scrolling="no" frameborder="0" width="300px" height="250px"></iframe></center>'; 
     }
 }
+//static
 if ($("#ads").length) {
        document.getElementById("ads").innerHTML = '<center><a href="http://corneey.com/w22j1h" target="_blank"><img src="https://rollercoin.com/static/img/referral/banners/ref_72890.gif" width="728" height="90" border="0" alt="" /></a></center>'; 
-    }
+}
