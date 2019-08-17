ee11d7ac045ea1f890f795a105b97d42c1989fd6
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2019-08-17 23:28:36 +0300

    Update static.js

--- static.js
+++ static.js
@@ -1,38 +1,38 @@
 (function() {
 if(document.getElementById("adsyed")){
-    var script = document.createElement('script');
-    script.dataset.cfasync = false;
-    script.async = true;
-    script.type = 'text/javascript';
-    script.src = '//p319089.clksite.com/adServe/banners?tid=319089_621614_0';
+    var scriptn = document.createElement('script');
+    scriptn.dataset.cfasync = false;
+    scriptn.async = true;
+    scriptn.type = 'text/javascript';
+    scriptn.src = '//p319089.clksite.com/adServe/banners?tid=319089_621614_0';
     var elem = document.getElementById("adsyed");
-    elem.appendChild(script);
+    elem.appendChild(scriptn);
 }
 if(document.getElementById("adsdort")){
-    var script = document.createElement('script');
-    script.dataset.cfasync = false;
-    script.async = true;
-    script.type = 'text/javascript';
-    script.src = '//p319089.clksite.com/adServe/banners?tid=319089_621614_1';
+    var scriptm = document.createElement('script');
+    scriptm.dataset.cfasync = false;
+    scriptm.async = true;
+    scriptm.type = 'text/javascript';
+    scriptm.src = '//p319089.clksite.com/adServe/banners?tid=319089_621614_1';
     var elem = document.getElementById("adsdort");
-    elem.appendChild(script);
+    elem.appendChild(scriptm);
 }
 if(document.getElementById("adsucyuz")){
-    var script = document.createElement('script');
-    script.dataset.cfasync = false;
-    script.async = true;
-    script.type = 'text/javascript';
-    script.src = '//p319089.clksite.com/adServe/banners?tid=319089_621614_2';
+    var scriptb = document.createElement('script');
+    scriptb.dataset.cfasync = false;
+    scriptb.async = true;
+    scriptb.type = 'text/javascript';
+    scriptb.src = '//p319089.clksite.com/adServe/banners?tid=319089_621614_2';
     var elem = document.getElementById("adsucyuz");
-    elem.appendChild(script);
+    elem.appendChild(scriptb);
 }
 if(document.getElementById("ads")) {
-    var script = document.createElement('script');
-    script.dataset.cfasync = false;
-    script.async = true;
-    script.type = 'text/javascript';
-    script.src = '//p319089.clksite.com/adServe/banners?tid=319089_621614_0';
+    var scriptv = document.createElement('script');
+    scriptv.dataset.cfasync = false;
+    scriptv.async = true;
+    scriptv.type = 'text/javascript';
+    scriptv.src = '//p319089.clksite.com/adServe/banners?tid=319089_621614_0';
     var elem = document.getElementById("ads");
-    elem.appendChild(script);
+    elem.appendChild(scriptv);
 }
 }).call(this);
