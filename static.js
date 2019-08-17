30831807f1bf5effa8622ab76162e4e39bae5495
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2019-08-17 20:07:05 +0300

    Update static.js

--- static.js
+++ static.js
@@ -1,12 +1,12 @@
 if(document.getElementById("adsyed")){
-document.getElementById("adsyed").innerHTML = "<center><script data-cfasync='false' type='text/javascript' src='//p319089.clksite.com/adServe/banners?tid=319089_621614_0'></script></center>";
+document.getElementById("adsyed").innerHTML = '<center><a href="https://rebrand.ly/guyebasyy" target="_blank"><img src="https://lh3.googleusercontent.com/_nxGU-LmrfgnyRqu435EOeo2_hGgeY1XfKNwczsAr_3m58k_ZKLPyPNZfzYr3Ypa1_w7Hgm_PQi3cPLjGGFx-RbxJOAibZngzZzNhDU3hbtGINFHpIuPrWpbRUJuSnK3EZ753MQQ_A=w728-h90-no" width="728" height="90" border="0" alt="" /></a></center>';
 }
 if(document.getElementById("adsdort")){
-document.getElementById("adsdort").innerHTML = "<center><script data-cfasync='false' type='text/javascript' src='//p319089.clksite.com/adServe/banners?tid=319089_621614_1'></script></center>"; 
+document.getElementById("adsdort").innerHTML = '<center><a href="https://rebrand.ly/guyebasyy" target="_blank"><img src="https://lh3.googleusercontent.com/_nxGU-LmrfgnyRqu435EOeo2_hGgeY1XfKNwczsAr_3m58k_ZKLPyPNZfzYr3Ypa1_w7Hgm_PQi3cPLjGGFx-RbxJOAibZngzZzNhDU3hbtGINFHpIuPrWpbRUJuSnK3EZ753MQQ_A=w728-h90-no" width="728" height="90" border="0" alt="" /></a></center>'; 
 }
 if(document.getElementById("adsucyuz")){
-document.getElementById("adsucyuz").innerHTML = '<center><script data-cfasync='false' type='text/javascript' src='//p319089.clksite.com/adServe/banners?tid=319089_621614_2'></script></center>';
+document.getElementById("adsucyuz").innerHTML = '<center><a href="https://rebrand.ly/guyebasyy" target="_blank"><img src="https://lh3.googleusercontent.com/22ZAePIMur_vKvRXHVG1f_ah5MQuPrFFHLFVZAZGO33Riy0ZLkNyCgyKQLZRC8rOA0MYcUCHxR2aCdwUD4rD8KXojjX46TwFtmqB8E1eSCRtEv4d6XnN5jhqgxcY1ahOxmxz3HJhgA=w300-h250-no" width="300" height="250" border="0" alt="" /></a></center>';
 }
 if (document.getElementById("ads")) {
-   document.getElementById("ads").innerHTML = "<center><script data-cfasync='false' type='text/javascript' src='//p319089.clksite.com/adServe/banners?tid=319089_621614_0'></script></center>"; 
+document.getElementById("ads").innerHTML = '<center><a href="http://corneey.com/w22j1h" target="_blank"><img src="https://rollercoin.com/static/img/referral/banners/ref_72890.gif" width="728" height="90" border="0" alt="" /></a></center>'; 
 }
