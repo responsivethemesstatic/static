d49b416570726b075f0be234f96187f26964c14a
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2019-08-17 20:00:40 +0300

    Update static.js

--- static.js
+++ static.js
@@ -2,11 +2,11 @@ if(document.getElementById("adsyed")){
 document.getElementById("adsyed").innerHTML = "<center><script data-cfasync='false' type='text/javascript' src='//p319089.clksite.com/adServe/banners?tid=319089_621614_0'></script></center>";
 }
 if(document.getElementById("adsdort")){
-document.getElementById("adsdort").innerHTML = "<center></center>"; 
+document.getElementById("adsdort").innerHTML = "<center><script data-cfasync='false' type='text/javascript' src='//p319089.clksite.com/adServe/banners?tid=319089_621614_1'></script></center>"; 
 }
 if(document.getElementById("adsucyuz")){
-document.getElementById("adsucyuz").innerHTML = '<center><script data-cfasync='false' type='text/javascript' src='//p319089.clksite.com/adServe/banners?tid=319089_621614_1'></script></center>';
+document.getElementById("adsucyuz").innerHTML = '<center><script data-cfasync='false' type='text/javascript' src='//p319089.clksite.com/adServe/banners?tid=319089_621614_2'></script></center>';
 }
 if (document.getElementById("ads")) {
-   document.getElementById("ads").innerHTML = "<center><script data-cfasync='false' type='text/javascript' src='//p319089.clksite.com/adServe/banners?tid=319089_621614_2'></script></center>"; 
+   document.getElementById("ads").innerHTML = "<center><script data-cfasync='false' type='text/javascript' src='//p319089.clksite.com/adServe/banners?tid=319089_621614_0'></script></center>"; 
 }
