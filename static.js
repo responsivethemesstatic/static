bb8b45b49f6da968cacf818de6db1710469352d5
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2019-08-25 03:49:48 +0300

    Update static.js

--- static.js
+++ static.js
@@ -1,14 +1,38 @@
-(function(){
+(function() {
 if(document.getElementById("adsyed")){
-document.getElementById("adsyed").innerHTML = '<center><a href="https://rebrand.ly/guyebasyy" target="_blank"><img src="https://lh3.googleusercontent.com/_nxGU-LmrfgnyRqu435EOeo2_hGgeY1XfKNwczsAr_3m58k_ZKLPyPNZfzYr3Ypa1_w7Hgm_PQi3cPLjGGFx-RbxJOAibZngzZzNhDU3hbtGINFHpIuPrWpbRUJuSnK3EZ753MQQ_A=w728-h90-no" width="728" height="90" border="0" alt="" /></a></center>';
+    var scriptn = document.createElement('script');
+    scriptn.dataset.cfasync = false;
+    scriptn.async = true;
+    scriptn.type = 'text/javascript';
+    scriptn.src = '//p319089.clksite.com/adServe/banners?tid=319089_621614_0';
+    var elemn = document.getElementById("adsyed");
+    elemn.appendChild(scriptn);
 }
 if(document.getElementById("adsdort")){
-document.getElementById("adsdort").innerHTML = '<center><a href="https://rebrand.ly/guyebasyy" target="_blank"><img src="https://lh3.googleusercontent.com/_nxGU-LmrfgnyRqu435EOeo2_hGgeY1XfKNwczsAr_3m58k_ZKLPyPNZfzYr3Ypa1_w7Hgm_PQi3cPLjGGFx-RbxJOAibZngzZzNhDU3hbtGINFHpIuPrWpbRUJuSnK3EZ753MQQ_A=w728-h90-no" width="728" height="90" border="0" alt="" /></a></center>'; 
+    var scriptm = document.createElement('script');
+    scriptm.dataset.cfasync = false;
+    scriptm.async = true;
+    scriptm.type = 'text/javascript';
+    scriptm.src = '//p319089.clksite.com/adServe/banners?tid=319089_621614_1';
+    var elemm = document.getElementById("adsdort");
+    elemm.appendChild(scriptm);
 }
 if(document.getElementById("adsucyuz")){
-document.getElementById("adsucyuz").innerHTML = '<center><a href="https://rebrand.ly/guyebasyy" target="_blank"><img src="https://lh3.googleusercontent.com/22ZAePIMur_vKvRXHVG1f_ah5MQuPrFFHLFVZAZGO33Riy0ZLkNyCgyKQLZRC8rOA0MYcUCHxR2aCdwUD4rD8KXojjX46TwFtmqB8E1eSCRtEv4d6XnN5jhqgxcY1ahOxmxz3HJhgA=w300-h250-no" width="300" height="250" border="0" alt="" /></a></center>';
+    var scriptb = document.createElement('script');
+    scriptb.dataset.cfasync = false;
+    scriptb.async = true;
+    scriptb.type = 'text/javascript';
+    scriptb.src = '//p319089.clksite.com/adServe/banners?tid=319089_621614_2';
+    var elemd = document.getElementById("adsucyuz");
+    elemd.appendChild(scriptb);
 }
 if (document.getElementById("ads")) {
-document.getElementById("ads").innerHTML = '<center><a href="https://rebrand.ly/guyebasyy" target="_blank"><img src="https://lh3.googleusercontent.com/_nxGU-LmrfgnyRqu435EOeo2_hGgeY1XfKNwczsAr_3m58k_ZKLPyPNZfzYr3Ypa1_w7Hgm_PQi3cPLjGGFx-RbxJOAibZngzZzNhDU3hbtGINFHpIuPrWpbRUJuSnK3EZ753MQQ_A=w728-h90-no" width="728" height="90" border="0" alt="" /></a></center>';
+    var scriptbv = document.createElement('script');
+    scriptbv.dataset.cfasync = false;
+    scriptbv.async = true;
+    scriptbv.type = 'text/javascript';
+    scriptbv.src = '//p319089.clksite.com/adServe/banners?tid=319089_621614_3';
+    var elemdv = document.getElementById("ads");
+    elemdv.appendChild(scriptbv);
 }
 }).call(this);
