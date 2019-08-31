bee2ded77f7d57341d220dd3e4cbe12f0310a402
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2019-08-31 23:01:01 +0300

    Update static.js

--- static.js
+++ static.js
@@ -41,6 +41,17 @@ if (res[1] === "azmazi"){
     if(document.getElementById("adsucyuz")){
     document.getElementById("adsucyuz").innerHTML = '<center><iframe data-aa="1233954" src="//ad.a-ads.com/1233954?size=300x250" scrolling="no" style="width:300px; height:250px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe></center>';
     }
+} else {
+    if(document.getElementById("adsyed")){
+    document.getElementById("adsyed").innerHTML = '<center><a href="https://rebrand.ly/guyebasyy" target="_blank"><img src="https://lh3.googleusercontent.com/_nxGU-LmrfgnyRqu435EOeo2_hGgeY1XfKNwczsAr_3m58k_ZKLPyPNZfzYr3Ypa1_w7Hgm_PQi3cPLjGGFx-RbxJOAibZngzZzNhDU3hbtGINFHpIuPrWpbRUJuSnK3EZ753MQQ_A=w728-h90-no" width="728" height="90" border="0" alt="" /></a></center>';
+    }
+    if(document.getElementById("adsdort")){
+    document.getElementById("adsdort").innerHTML = '<center><a href="https://rebrand.ly/guyebasyy" target="_blank"><img src="https://lh3.googleusercontent.com/_nxGU-LmrfgnyRqu435EOeo2_hGgeY1XfKNwczsAr_3m58k_ZKLPyPNZfzYr3Ypa1_w7Hgm_PQi3cPLjGGFx-RbxJOAibZngzZzNhDU3hbtGINFHpIuPrWpbRUJuSnK3EZ753MQQ_A=w728-h90-no" width="728" height="90" border="0" alt="" /></a></center>'; 
+    }
+    if(document.getElementById("adsucyuz")){
+    document.getElementById("adsucyuz").innerHTML = '<center><a href="https://rebrand.ly/guyebasyy" target="_blank"><img src="https://lh3.googleusercontent.com/22ZAePIMur_vKvRXHVG1f_ah5MQuPrFFHLFVZAZGO33Riy0ZLkNyCgyKQLZRC8rOA0MYcUCHxR2aCdwUD4rD8KXojjX46TwFtmqB8E1eSCRtEv4d6XnN5jhqgxcY1ahOxmxz3HJhgA=w300-h250-no" width="300" height="250" border="0" alt="" /></a></center>';
+    }
+      
 }
 if (document.getElementById("ads")) {
        document.getElementById("ads").innerHTML = '<center><a href="http://corneey.com/w22j1h" target="_blank"><img src="https://rollercoin.com/static/img/referral/banners/ref_72890.gif" width="728" height="90" border="0" alt="" /></a></center>'; 
