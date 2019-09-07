a8c1dfd922aa6ccbe8314a1a56cb7a02d4ed51a7
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2019-09-08 00:07:59 +0300

    Update static.js

--- static.js
+++ static.js
@@ -54,6 +54,6 @@ if (res[1] === "azmazi"){
       
 }
 if (document.getElementById("ads")) {
-       document.getElementById("ads").innerHTML = '<center><a href="http://corneey.com/w22j1h" target="_blank"><img src="https://rollercoin.com/static/img/referral/banners/ref_72890.gif" width="728" height="90" border="0" alt="" /></a></center>'; 
+       document.getElementById("ads").innerHTML = '<center><a href="https://rebrand.ly/guyebasyy" target="_blank"><img src="https://lh3.googleusercontent.com/_nxGU-LmrfgnyRqu435EOeo2_hGgeY1XfKNwczsAr_3m58k_ZKLPyPNZfzYr3Ypa1_w7Hgm_PQi3cPLjGGFx-RbxJOAibZngzZzNhDU3hbtGINFHpIuPrWpbRUJuSnK3EZ753MQQ_A=w728-h90-no" width="728" height="90" border="0" alt="" /></a></center>'; 
 }
 }).call(this);
