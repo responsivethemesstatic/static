7a5b57da63c767428d74abaef7ed1a312e16b3d5
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2019-09-11 02:58:04 +0300

    Update static.js

--- static.js
+++ static.js
@@ -1,49 +1,6 @@
 (function(){
-var loc= window.location.href.toString(); 
-var res = loc.match(/http.*:\/\/(.*).blogspot./);
-if (res[1] === "azmazi"){
-    if(document.getElementById("adsyed")){
-    document.getElementById("adsyed").innerHTML = '<center><iframe data-aa="1233940" src="//ad.a-ads.com/1233940?size=728x90" scrolling="no" style="width:728px; height:90px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe></center>';
-    }
-    if(document.getElementById("adsdort")){
-    document.getElementById("adsdort").innerHTML = '<center><iframe data-aa="1233941" src="//ad.a-ads.com/1233941?size=468x60" scrolling="no" style="width:468px; height:60px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe></center>'; 
-    }
-    if(document.getElementById("adsucyuz")){
-    document.getElementById("adsucyuz").innerHTML = '<center><iframe data-aa="1233942" src="//ad.a-ads.com/1233942?size=300x250" scrolling="no" style="width:300px; height:250px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe></center>';
-    }
-} else if (res[1] === "gladyshep"){
-    if(document.getElementById("adsyed")){
-    document.getElementById("adsyed").innerHTML = '<center><iframe data-aa="1233944" src="//ad.a-ads.com/1233944?size=728x90" scrolling="no" style="width:728px; height:90px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe></center>';
-    }
-    if(document.getElementById("adsdort")){
-    document.getElementById("adsdort").innerHTML = '<center><iframe data-aa="1233945" src="//ad.a-ads.com/1233945?size=468x60" scrolling="no" style="width:468px; height:60px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe></center>'; 
-    }
-    if(document.getElementById("adsucyuz")){
-    document.getElementById("adsucyuz").innerHTML = '<center><iframe data-aa="1233947" src="//ad.a-ads.com/1233947?size=300x250" scrolling="no" style="width:300px; height:250px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe></center>';
-    }
-} else if (res[1] === "hayhonsu"){
-    if(document.getElementById("adsyed")){
-    document.getElementById("adsyed").innerHTML = '<center><iframe data-aa="1233949" src="//ad.a-ads.com/1233949?size=728x90" scrolling="no" style="width:728px; height:90px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe></center>';
-    }
-    if(document.getElementById("adsdort")){
-    document.getElementById("adsdort").innerHTML = '<center><iframe data-aa="1233950" src="//ad.a-ads.com/1233950?size=468x60" scrolling="no" style="width:468px; height:60px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe></center>'; 
-    }
-    if(document.getElementById("adsucyuz")){
-    document.getElementById("adsucyuz").innerHTML = '<center><iframe data-aa="1233951" src="//ad.a-ads.com/1233951?size=300x250" scrolling="no" style="width:300px; height:250px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe></center>';
-    }
-} else if (res[1] === "ayzwa"){
-    if(document.getElementById("adsyed")){
-    document.getElementById("adsyed").innerHTML = '<center><iframe data-aa="1233952" src="//ad.a-ads.com/1233952?size=728x90" scrolling="no" style="width:728px; height:90px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe></center>';
-    }
-    if(document.getElementById("adsdort")){
-    document.getElementById("adsdort").innerHTML = '<center><iframe data-aa="1233953" src="//ad.a-ads.com/1233953?size=468x60" scrolling="no" style="width:468px; height:60px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe></center>'; 
-    }
-    if(document.getElementById("adsucyuz")){
-    document.getElementById("adsucyuz").innerHTML = '<center><iframe data-aa="1233954" src="//ad.a-ads.com/1233954?size=300x250" scrolling="no" style="width:300px; height:250px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe></center>';
-    }
-} else {
-    if(document.getElementById("adsyed")){
-    document.getElementById("adsyed").innerHTML = '<center><a href="https://rebrand.ly/riller" target="_blank"><img src="https://rollercoin.com/static/img/referral/banners/ref_72890.png" width="728" height="90" border="0" alt="" /></a></center>';
+	if(document.getElementById("adsyed")){
+	document.getElementById("adsyed").innerHTML = '<center><a href="https://rebrand.ly/riller" target="_blank"><img src="https://rollercoin.com/static/img/referral/banners/ref_72890.png" width="728" height="90" border="0" alt="" /></a></center>';
     }
     if(document.getElementById("adsdort")){
     document.getElementById("adsdort").innerHTML = '<center><a href="https://rebrand.ly/riller" target="_blank"><img src="https://rollercoin.com/static/img/referral/banners/ref_72890.png" width="728" height="90" border="0" alt="" /></a></center>'; 
@@ -51,9 +8,7 @@ if (res[1] === "azmazi"){
     if(document.getElementById("adsucyuz")){
     document.getElementById("adsucyuz").innerHTML = '<center><a href="https://rebrand.ly/riller" target="_blank"><img src="https://rollercoin.com/static/img/referral/banners/ref_336280.png" width="300" height="250" border="0" alt="" /></a></center>';
     }
-      
-}
-if (document.getElementById("ads")) {
+	if (document.getElementById("ads")) {
        document.getElementById("ads").innerHTML = '<center><a href="https://rebrand.ly/riller" target="_blank"><img src="https://rollercoin.com/static/img/referral/banners/ref_72890.png" width="728" height="90" border="0" alt="" /></a></center>'; 
-}
+	}
 }).call(this);
