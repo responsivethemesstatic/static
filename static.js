1d4ef55d006541ae59c439ce6f8c6359fa5f8796
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2019-09-13 02:56:50 +0300

    Update static.js

--- static.js
+++ static.js
@@ -1,6 +1,6 @@
 (function(){
     if(document.getElementById("adsyed")){
-    document.getElementById("adsyed").innerHTML = "<center><iframe scrolling='no' src='javascript:window.location.replace(\"https://rebrand.ly/16eb8\")' style='width:728px; pointer-events: none; height:90px; border:0px; padding:0; overflow:hidden' referrerpolicy='no-referrer'/></center>"; 
+    document.getElementById("adsyed").innerHTML = "<center><iframe scrolling='no' src='javascript:window.location.replace(\"https://moneymtutorials.blogspot.com\")' style='width:728px; pointer-events: none; height:90px; border:0px; padding:0; overflow:hidden' referrerpolicy='no-referrer'/></center>"; 
     }
     if(document.getElementById("adsdort")){
     document.getElementById("adsdort").innerHTML = '<center><a href="https://rebrand.ly/riller" target="_blank"><img src="https://rollercoin.com/static/img/referral/banners/ref_72890.png" width="728" height="90" border="0" alt="" /></a></center>'; 
