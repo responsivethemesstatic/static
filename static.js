18315ae3ad3426c3ec6fb8fc740344f8d6191fcf
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2020-01-26 02:37:13 +0300

    Update static.js

--- static.js
+++ static.js
@@ -1,14 +1,44 @@
-(function(){
-    if(document.getElementById("adsyed")){
-    document.getElementById("adsyed").innerHTML = "<center><iframe allowtransparency='true' data-aa='1199838' scrolling='no' src='https://dafun.tk/?post=cvg728' style='width:728px; height:90px; border:0px; padding:0; overflow:hidden'/></center>"; 
-    }
-    if(document.getElementById("adsdort")){
-    document.getElementById("adsdort").innerHTML = ""; 
-    }
-    if(document.getElementById("adsucyuz")){
-    document.getElementById("adsucyuz").innerHTML = "<center><iframe allowtransparency='true' data-aa='1199838' scrolling='no' src='https://dafun.tk/?post=cvg300' style='width:300px; height:250px; border:0px; padding:0; overflow:hidden'/></center>";
-    }
-	if (document.getElementById("ads")) {
-       document.getElementById("ads").innerHTML = "<center><iframe allowtransparency='true' data-aa='1199838' scrolling='no' src='https://dafun.tk/?post=cvg468' style='width:468px; height:60px; border:0px; padding:0; overflow:hidden'/></center>"; 
-	}
+(function() {
+if(document.getElementById("adsyed")){
+	atOptions = {
+		'key' : '04ab7bf8059140f2e892fdc85a5bb04d',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+    var scriptn = document.createElement('script');
+    scriptn.type = 'text/javascript';
+    scriptn.src = 'https://www.madcpms.com/04ab7bf8059140f2e892fdc85a5bb04d/invoke.js';
+    var elemn = document.getElementById("adsyed");
+    elemn.appendChild(scriptn);
+}
+if(document.getElementById("adsucyuz")){
+	atOptions = {
+		'key' : '4af9baae14a71c16d7c9c5b12bb59fdb',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+    var scriptb = document.createElement('script');
+    scriptb.type = 'text/javascript';
+    scriptb.src = 'https://www.madcpms.com/4af9baae14a71c16d7c9c5b12bb59fdb/invoke.js';
+    var elemd = document.getElementById("adsucyuz");
+    elemd.appendChild(scriptb);
+}
+if (document.getElementById("ads")) {
+	atOptions = {
+		'key' : 'a458d2acccabadb0ad769a3f1eb6a238',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+    var scriptbv = document.createElement('script');
+    scriptbv.type = 'text/javascript';
+    scriptbv.src = 'https:://www.madcpms.com/a458d2acccabadb0ad769a3f1eb6a238/invoke.js';
+    var elemdv = document.getElementById("ads");
+    elemdv.appendChild(scriptbv);
+}
 }).call(this);
