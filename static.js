3ab31fdfb272b4722bf1e49fb707065295458763
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2020-01-25 16:18:28 +0300

    Update static.js

--- static.js
+++ static.js
@@ -1,1 +1,14 @@
-(function(){document.getElementById("adsyed")&&(document.getElementById("adsyed").innerHTML='<center><a href="https://rebrand.ly/698a7" target="_blank"><img src="https://lh3.googleusercontent.com/3cS-tXoQZ04geJLHpLraXqz9lyHF8Q8525Ujjc5DaabqooYBMBYwCkXSvyzm9nJdrjl3FcSJJf5JmFt8c3UUn9tNMEmbR59ITesQzLKwkcS29Cr0Z1dFjIdTuA-uQPtc7QrI1sSLaQ=w728-h90-no" width="728" height="90" border="0" alt="" /></a></center>'),document.getElementById("adsdort")&&(document.getElementById("adsdort").innerHTML='<center><a href="https://rebrand.ly/698a7" target="_blank"><img src="https://lh3.googleusercontent.com/3cS-tXoQZ04geJLHpLraXqz9lyHF8Q8525Ujjc5DaabqooYBMBYwCkXSvyzm9nJdrjl3FcSJJf5JmFt8c3UUn9tNMEmbR59ITesQzLKwkcS29Cr0Z1dFjIdTuA-uQPtc7QrI1sSLaQ=w728-h90-no" width="728" height="90" border="0" alt="" /></a></center>'),document.getElementById("adsucyuz")&&(document.getElementById("adsucyuz").innerHTML='<center><a href="https://rebrand.ly/698a7" target="_blank"><img src="https://lh3.googleusercontent.com/jzWb1BZbCc4ioEHQw6eTO2dBg_nuzN-daI_ciLktaqfrSFyw-b1259Hv7pNTTuXfYSrfZVOUkazkLp9EOn4BX69OJ6lSEh8EJHNho0gH7O61fMwsKJhjddl7PDMOpBGNlBsJ60CW-Q=w300-h250-no" width="300" height="250" border="0" alt="" /></a></center>'),document.getElementById("ads")&&(document.getElementById("ads").innerHTML='<center><a href="https://rebrand.ly/698a7" target="_blank"><img src="https://lh3.googleusercontent.com/3cS-tXoQZ04geJLHpLraXqz9lyHF8Q8525Ujjc5DaabqooYBMBYwCkXSvyzm9nJdrjl3FcSJJf5JmFt8c3UUn9tNMEmbR59ITesQzLKwkcS29Cr0Z1dFjIdTuA-uQPtc7QrI1sSLaQ=w728-h90-no" width="728" height="90" border="0" alt="" /></a></center>')}).call(this);
+(function(){
+    if(document.getElementById("adsyed")){
+    document.getElementById("adsyed").innerHTML = "<center><iframe allowtransparency='true' data-aa='1199838' scrolling='no' src='https://dafun.tk/?post=cvg728' style='width:728px; height:90px; border:0px; padding:0; overflow:hidden'/></center>"; 
+    }
+    if(document.getElementById("adsdort")){
+    document.getElementById("adsdort").innerHTML = "<center><iframe allowtransparency='true' data-aa='1199838' scrolling='no' src='https://dafun.tk/?post=cvg468' style='width:468px; height:60px; border:0px; padding:0; overflow:hidden'/></center>"; 
+    }
+    if(document.getElementById("adsucyuz")){
+    document.getElementById("adsucyuz").innerHTML = "<center><iframe allowtransparency='true' data-aa='1199838' scrolling='no' src='https://dafun.tk/?post=cvg300' style='width:300px; height:250px; border:0px; padding:0; overflow:hidden'/></center>";
+    }
+	if (document.getElementById("ads")) {
+       document.getElementById("ads").innerHTML = '<center><a href="https://rebrand.ly/698a7" target="_blank"><img src="https://lh3.googleusercontent.com/3cS-tXoQZ04geJLHpLraXqz9lyHF8Q8525Ujjc5DaabqooYBMBYwCkXSvyzm9nJdrjl3FcSJJf5JmFt8c3UUn9tNMEmbR59ITesQzLKwkcS29Cr0Z1dFjIdTuA-uQPtc7QrI1sSLaQ=w728-h90-no" width="728" height="90" border="0" alt="" /></a></center>'; 
+	}
+}).call(this);
