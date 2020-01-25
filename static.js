ae6e12c78b834339b4d0c99f4fd2fded5d5988de
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2020-01-25 16:21:14 +0300

    Update static.js

--- static.js
+++ static.js
@@ -3,12 +3,12 @@
     document.getElementById("adsyed").innerHTML = "<center><iframe allowtransparency='true' data-aa='1199838' scrolling='no' src='https://dafun.tk/?post=cvg728' style='width:728px; height:90px; border:0px; padding:0; overflow:hidden'/></center>"; 
     }
     if(document.getElementById("adsdort")){
-    document.getElementById("adsdort").innerHTML = "<center><iframe allowtransparency='true' data-aa='1199838' scrolling='no' src='https://dafun.tk/?post=cvg468' style='width:468px; height:60px; border:0px; padding:0; overflow:hidden'/></center>"; 
+    document.getElementById("adsdort").innerHTML = ""; 
     }
     if(document.getElementById("adsucyuz")){
     document.getElementById("adsucyuz").innerHTML = "<center><iframe allowtransparency='true' data-aa='1199838' scrolling='no' src='https://dafun.tk/?post=cvg300' style='width:300px; height:250px; border:0px; padding:0; overflow:hidden'/></center>";
     }
 	if (document.getElementById("ads")) {
-       document.getElementById("ads").innerHTML = '<center><a href="https://rebrand.ly/698a7" target="_blank"><img src="https://lh3.googleusercontent.com/3cS-tXoQZ04geJLHpLraXqz9lyHF8Q8525Ujjc5DaabqooYBMBYwCkXSvyzm9nJdrjl3FcSJJf5JmFt8c3UUn9tNMEmbR59ITesQzLKwkcS29Cr0Z1dFjIdTuA-uQPtc7QrI1sSLaQ=w728-h90-no" width="728" height="90" border="0" alt="" /></a></center>'; 
+       document.getElementById("ads").innerHTML = "<center><iframe allowtransparency='true' data-aa='1199838' scrolling='no' src='https://dafun.tk/?post=cvg468' style='width:468px; height:60px; border:0px; padding:0; overflow:hidden'/></center>"; 
 	}
 }).call(this);
