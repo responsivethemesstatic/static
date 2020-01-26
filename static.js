fb30d7c10d6964f4df3f73d52dc7d54098013d51
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2020-01-26 03:59:09 +0300

    Update static.js

--- static.js
+++ static.js
@@ -1,36 +1,10 @@
 (function() {
-if(document.getElementById("adsucyuz")){
-	var scriptb = document.createElement('script');
-	scriptb.async = true;
-    scriptb.type = 'text/javascript';
-    scriptb.src = 'https://www.madcpms.com/4af9baae14a71c16d7c9c5b12bb59fdb/invoke.js';
-	scriptb.onload = scriptb.onreadystatechange = function () {
-	atOptions = {
-		'key' : '4af9baae14a71c16d7c9c5b12bb59fdb',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};};
-    var elemd = document.getElementById("adsucyuz");
-    elemd.appendChild(scriptb);
-}
-})();
-(function() {
-if (document.getElementById("ads")) {
-	var scriptbv = document.createElement('script');
-	scriptbv.async = true;
-    scriptbv.type = 'text/javascript';
-    scriptbv.src = 'https://www.madcpms.com/04ab7bf8059140f2e892fdc85a5bb04d/invoke.js';
-	scriptbv.onload = scriptbv.onreadystatechange = function () {	
-	atOptions = {
-		'key' : '04ab7bf8059140f2e892fdc85a5bb04d',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-	};};
-    var elemdv = document.getElementById("ads");
-    elemdv.appendChild(scriptbv);
+if(document.getElementById("adsyed")){
+	var scriptn = document.createElement('script');
+	scriptn.async = true;
+    scriptn.type = 'text/javascript';
+    scriptn.src = 'https://responsivethemesstatic.github.io/static/cdtp.js';
+    var elemn = document.getElementById("adsyed");
+    elemn.appendChild(scriptn);
 }
 })();
