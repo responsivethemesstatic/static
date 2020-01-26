b81a9ac712e523daefcb16af93aeb96cda28b72d
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2020-01-26 10:35:04 +0300

    Update static.js

--- static.js
+++ static.js
@@ -9,8 +9,24 @@ if(document.getElementById("adsyed")){
 	script.defer = true;
     script.type = 'text/javascript';
     script.src = 'https://www.madcpms.com/04ab7bf8059140f2e892fdc85a5bb04d/invoke.js';
+	script.onload = scriptn.onreadystatechange = function () {myFnion();};
     var el = document.getElementById("adsyed");
     el.appendChild(script);
 }
 })();
+function myFnion() {
+if(document.getElementById("adsucyuz")){
+	var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.text = "atOptions = {'key' : '4af9baae14a71c16d7c9c5b12bb59fdb','format' : 'iframe','height' : 250,'width' : 300,'params' : {} 	};";
+	var el = document.getElementById("adsucyuz");
+    el.appendChild(script);
+	var script = document.createElement('script');
+	script.defer = true;
+    script.type = 'text/javascript';
+    script.src = 'https://www.madcpms.com/4af9baae14a71c16d7c9c5b12bb59fdb/invoke.js';
+    var el = document.getElementById("adsucyuz");
+    el.appendChild(script);
+}
+}
 
