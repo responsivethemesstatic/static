47ebee704a85cf3ad4c1d940e74a2cec76244921
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2020-01-26 04:01:02 +0300

    Update cdtp.js

--- cdtp.js
+++ cdtp.js
@@ -1,5 +1,4 @@
-<script type="text/javascript">
-	atOptions = {
+atOptions = {
 		'key' : '04ab7bf8059140f2e892fdc85a5bb04d',
 		'format' : 'iframe',
 		'height' : 90,
@@ -7,4 +6,3 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.madcpms.com/04ab7bf8059140f2e892fdc85a5bb04d/invoke.js"></scr' + 'ipt>');
-</script>
