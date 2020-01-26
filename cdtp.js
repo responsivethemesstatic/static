6ca0b0d039e046a46a884e6f7bfe14147c49d2e8
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2020-01-26 03:57:13 +0300

    Create cdtp.js

---
+++ cdtp.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '04ab7bf8059140f2e892fdc85a5bb04d',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.madcpms.com/04ab7bf8059140f2e892fdc85a5bb04d/invoke.js"></scr' + 'ipt>');
+</script>
