bd04c0cf4185a4db1af93b2aa7660e2be65581b0
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2020-01-26 10:50:13 +0300

    Update static.js

--- static.js
+++ static.js
@@ -6,6 +6,7 @@ if(document.getElementById("adsyed")){
 	var el = document.getElementById("adsyed");
     el.appendChild(script);
 	var script = document.createElement('script');
+	script.async = true;
     script.type = 'text/javascript';
     script.src = 'https://www.madcpms.com/04ab7bf8059140f2e892fdc85a5bb04d/invoke.js';
 	script.onload = scriptn.onreadystatechange = function () {myFnion();};
@@ -21,6 +22,7 @@ if(document.getElementById("adsucyuz")){
 	var el = document.getElementById("adsucyuz");
     el.appendChild(script);
 	var script = document.createElement('script');
+	script.async = true;
     script.type = 'text/javascript';
     script.src = 'https://www.madcpms.com/4af9baae14a71c16d7c9c5b12bb59fdb/invoke.js';
     var el = document.getElementById("adsucyuz");
