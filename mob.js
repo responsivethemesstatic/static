7f8727101b1f479352120a9384f63e3e57e3d501
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2020-01-27 15:45:55 +0300

    Update mob.js

--- mob.js
+++ mob.js
@@ -4,8 +4,7 @@
     script.async = true;
     script.type = 'text/javascript';
     script.src = 'https://responsivethemesstatic.github.io/static/static.js';
-    var entry = document.getElementsByTagName('script')[0];
-    entry.parentNode.insertBefore(script, entry);
+    document.body.appendChild(script);
 })();
 (function() {
 
