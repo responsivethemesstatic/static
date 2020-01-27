4482a6cbe90dc7af29e690bd982217fc7c6bc523
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2020-01-27 15:27:56 +0300

    Delete side.js

--- side.js
+++
@@ -1,8 +0,0 @@
-	atOptions = {
-		'key' : '4af9baae14a71c16d7c9c5b12bb59fdb',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.madcpms.com/4af9baae14a71c16d7c9c5b12bb59fdb/invoke.js"></scr' + 'ipt>');
