a162126f5a466986b8eeb644929969653b7f3721
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2020-06-17 17:19:30 +0300

    Update mob.js

--- mob.js
+++ mob.js
@@ -7,7 +7,6 @@
 })();
 (function() {
  var script = document.createElement('script');
-    script.async = true;
     script.type = 'text/javascript';
     script.src = '//pl15563170.passtechusa.com/35/da/1e/35da1e3af13599295821b2545bdc31fc.js';
     document.body.appendChild(script);
