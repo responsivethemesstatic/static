d2105224e5a3f21e3e4c64a1a4a04e50649bf5ee
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2021-09-01 05:27:28 +0300

    Update res.js

--- res.js
+++ res.js
@@ -1,28 +1,19 @@
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-function ucheck(){
-        var botPattern = "(googlebot\/|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis|Pinterestbot|bot.html)";
-          var re = new RegExp(botPattern, 'i');
-          var userAgent = navigator.userAgent;
-          if (re.test(userAgent)) {
-              return true;
-          }else{
-            return false;
-          }
-}
+
 async function relCall() {
         var script, el;
         await sleep(1000);
         if(document.getElementById("top_720")){
             var script = document.createElement('script');
             script.type = 'text/javascript';
-            script.text = "atOptions = {'key' : '04ab7bf8059140f2e892fdc85a5bb04d','format' : 'iframe','height' : 90,'width' : 728,'params' : {} 	};";
+            script.text = "atOptions = {'key' : '9517a4efa0af3fead641c773740cb25f','format' : 'iframe','height' : 90,'width' : 728,'params' : {} 	};";
             var el = document.getElementById("top_720");
             el.appendChild(script);
             var script = document.createElement('script');
             script.type = 'text/javascript';
-            script.src = 'https://www.variouscreativeformats.com/04ab7bf8059140f2e892fdc85a5bb04d/invoke.js';
+            script.src = 'https://www.highperformancedformats.com/9517a4efa0af3fead641c773740cb25f/invoke.js';
             var el = document.getElementById("top_720");
             el.appendChild(script);
         }
@@ -30,12 +21,12 @@ async function relCall() {
         if(document.getElementById("sidebar_300")){
             var script = document.createElement('script');
             script.type = 'text/javascript';
-            script.text = "atOptions = {'key' : '4af9baae14a71c16d7c9c5b12bb59fdb','format' : 'iframe','height' : 250,'width' : 300,'params' : {} 	};";
+            script.text = "atOptions = {'key' : '90073778d39cc0a40947c56562a933d5','format' : 'iframe','height' : 250,'width' : 300,'params' : {} 	};";
             var el = document.getElementById("sidebar_300");
             el.appendChild(script);
             var script = document.createElement('script');
             script.type = 'text/javascript';
-            script.src = 'https://www.variouscreativeformats.com/4af9baae14a71c16d7c9c5b12bb59fdb/invoke.js';
+            script.src = 'https://www.highperformancedformats.com/90073778d39cc0a40947c56562a933d5/invoke.js';
             var el = document.getElementById("sidebar_300");
             el.appendChild(script);
         }
@@ -47,23 +38,21 @@ async function relCall() {
             el.appendChild(script);
             var script = document.createElement('script');
             script.type = 'text/javascript';
-            script.text = "atOptions = {'key' : 'a458d2acccabadb0ad769a3f1eb6a238','format' : 'iframe','height' : 60,'width' : 468,'params' : {} 	};";
+            script.text = "atOptions = {'key' : '3a70c20c4dd4363472d820cab195c162','format' : 'iframe','height' : 60,'width' : 468,'params' : {} 	};";
             var el = document.getElementById("a458d2acc");
             el.appendChild(script);
             var script = document.createElement('script');
             script.type = 'text/javascript';
-            script.src = 'https://www.variouscreativeformats.com/a458d2acccabadb0ad769a3f1eb6a238/invoke.js';
+            script.src = 'https://www.highperformancedformats.com/3a70c20c4dd4363472d820cab195c162/invoke.js';
             var el = document.getElementById("a458d2acc");
             el.appendChild(script);
         }
 }
-if(ucheck() === false){
-    (function() {
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = '//pl15563170.alternativecpmgate.com/35/da/1e/35da1e3af13599295821b2545bdc31fc.js';
-        document.body.appendChild(script);
-    })();
-    
-    relCall();
-}
+(function() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//pl16561963.effectivecpmgate.com/f5/73/2f/f5732f8ef13b81d67efb19cf1589fed0.js';
+    document.body.appendChild(script);
+})();
+
+relCall();
