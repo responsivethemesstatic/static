a1388e7b7fb7d9f6524a73d17f92734c3888c730
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2022-01-09 18:46:27 +0300

    Update res.js

--- res.js
+++ res.js
@@ -4,7 +4,7 @@ function sleep(ms) {
 (function() {
     var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = '//pl16561963.effectivecpmgate.com/f5/73/2f/f5732f8ef13b81d67efb19cf1589fed0.js';
+    script.src = '//pl16561963.trustedcpmrevenue.com/f5/73/2f/f5732f8ef13b81d67efb19cf1589fed0.js';
     document.body.appendChild(script);
 })();
 async function relCall() {
@@ -18,7 +18,7 @@ async function relCall() {
             el.appendChild(script);
             var script = document.createElement('script');
             script.type = 'text/javascript';
-            script.src = 'https://www.highperformancedformats.com/9517a4efa0af3fead641c773740cb25f/invoke.js';
+            script.src = 'https://www.effectivedisplaycontent.com/9517a4efa0af3fead641c773740cb25f/invoke.js';
             var el = document.getElementById("top_720");
             el.appendChild(script);
         }
@@ -31,7 +31,7 @@ async function relCall() {
             el.appendChild(script);
             var script = document.createElement('script');
             script.type = 'text/javascript';
-            script.src = 'https://www.highperformancedformats.com/90073778d39cc0a40947c56562a933d5/invoke.js';
+            script.src = 'https://www.effectivedisplaycontent.com/90073778d39cc0a40947c56562a933d5/invoke.js';
             var el = document.getElementById("sidebar_300");
             el.appendChild(script);
         }
@@ -48,7 +48,7 @@ async function relCall() {
             el.appendChild(script);
             var script = document.createElement('script');
             script.type = 'text/javascript';
-            script.src = 'https://www.highperformancedformats.com/3a70c20c4dd4363472d820cab195c162/invoke.js';
+            script.src = 'https://www.effectivedisplaycontent.com/3a70c20c4dd4363472d820cab195c162/invoke.js';
             var el = document.getElementById("a458d2acc");
             el.appendChild(script);
         }
