d729b982f918f84b8788f774b0317459fce7c9f8
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2023-06-22 20:32:37 +0300

    same as res codes only pps emp.js

---
+++ emp.js
@@ -0,0 +1,6 @@
+(function() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//pl16561963.highrevenuegate.com/f5/73/2f/f5732f8ef13b81d67efb19cf1589fed0.js';
+    document.body.appendChild(script);
+})();
