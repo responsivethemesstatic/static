9ce1f2ca8052997d18b2d17f5ec72f5a5fdf4a98
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2026-03-14 18:01:57 +0500

    Update wp.js

--- wp.js
+++ wp.js
@@ -1,60 +1,57 @@
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
-if(document.getElementById("extra_bottomads")){
-    document.getElementById("extra_bottomads").innerHTML='<a href="https://bit.ly/39sJ4zI"><img title="Join Cointiply to Earn Free Bitcoin!" alt="Earn Free Bitcoin" src="https://cointiply.com/img/promo/banners/1_728x90.gif" width=600" height="80"></a>';
-}
-(function() {
-var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = '//pl15606332.highrevenuegate.com/b1/ac/56/b1ac56cbcd08bd11bfd56153b48b6533.js';
-  document.body.appendChild(script);
-})();
-async function relCall() {
-        var script, el;
-        await sleep(1000);
-        if(document.getElementById("top_720")){
-            script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.text = "atOptions = {'key' : '4816f97e953f99fa655fdcdd0c4f5c71','format' : 'iframe','height' : 90,'width' : 728,'params' : {} 	};";
-            el = document.getElementById("top_720");
-            el.appendChild(script);
-            script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.src = 'https://www.profitabledisplaynetwork.com/4816f97e953f99fa655fdcdd0c4f5c71/invoke.js';
-            el = document.getElementById("top_720");
-            el.appendChild(script);
-        }
-        await sleep(3000);
-        if(document.getElementById("sidebar_300")){
-            script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.text = "atOptions = {'key' : 'eb5846df10a6486393481a40d1b87eb4','format' : 'iframe','height' : 250,'width' : 300,'params' : {} 	};";
-            el = document.getElementById("sidebar_300");
-            el.appendChild(script);
-            script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.src = 'https://www.profitabledisplaynetwork.com/eb5846df10a6486393481a40d1b87eb4/invoke.js';
-            el = document.getElementById("sidebar_300");
-            el.appendChild(script);
-        }
-        await sleep(3000);
-        if(document.getElementById("in_post_bottomads")){
-            script = document.createElement('center');
-            script.id = 'a458d2acc';
-            el = document.getElementById("in_post_bottomads");
-            el.appendChild(script);
-            script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.text = "atOptions = {'key' : '1bf289e4ec5b58c423a7597677220242','format' : 'iframe','height' : 60,'width' : 468,'params' : {} 	};";
-            el = document.getElementById("a458d2acc");
-            el.appendChild(script);
-            script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.src = 'https://www.profitabledisplaynetwork.com/1bf289e4ec5b58c423a7597677220242/invoke.js';
-            el = document.getElementById("a458d2acc");
-            el.appendChild(script);
-        }
-}
-relCall();
 
+async function relCall(){
+
+    await sleep(1000);
+	try {
+		(function () {
+			const script = document.createElement("script");
+			script.src = "https://pl15606332.effectivegatecpm.com/b1/ac/56/b1ac56cbcd08bd11bfd56153b48b6533.js";
+			(document.body || document.documentElement).appendChild(script);
+		})();
+	} catch (e) {}
+	
+    let el = document.getElementById("top_720");
+    if(el){
+        let s1 = document.createElement("script");
+        s1.innerHTML = "window.atOptions={key:'4816f97e953f99fa655fdcdd0c4f5c71',format:'iframe',height:90,width:728,params:{}};";
+        el.appendChild(s1);
+
+        let s2 = document.createElement("script");
+        s2.src = "https://www.highperformanceformat.com/4816f97e953f99fa655fdcdd0c4f5c71/invoke.js";
+        el.appendChild(s2);
+    }
+
+    el = document.getElementById("sidebar_300");
+    if(el){
+        let s1 = document.createElement("script");
+        s1.innerHTML = "window.atOptions={key:'eb5846df10a6486393481a40d1b87eb4',format:'iframe',height:250,width:300,params:{}};";
+        el.appendChild(s1);
+
+        let s2 = document.createElement("script");
+        s2.src = "https://www.highperformanceformat.com/eb5846df10a6486393481a40d1b87eb4/invoke.js";
+        el.appendChild(s2);
+    }
+
+	var parent = document.getElementById("in_post_bottomads");
+
+	if(parent){
+
+		var container = document.createElement("div");
+		container.id = "a458d2acc";
+		
+		parent.appendChild(container);
+
+		var s1 = document.createElement("script");
+		s1.innerHTML = "window.atOptions={'key':'1bf289e4ec5b58c423a7597677220242','format':'iframe','height':60,'width':468,'params':{}};";
+		container.appendChild(s1);
+
+		var s2 = document.createElement("script");
+		s2.src = "https://www.highperformanceformat.com/1bf289e4ec5b58c423a7597677220242/invoke.js";
+		container.appendChild(s2);
+	}
+}
+
+document.addEventListener("DOMContentLoaded", relCall);
