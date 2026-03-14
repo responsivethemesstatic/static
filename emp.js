dd48783fb8c8ad26834bd7fc2489f2e0169d1bc9
responsivethemesstatic <53955700+responsivethemesstatic@users.noreply.github.com>
2026-03-14 18:04:22 +0500

    Update emp.js

--- emp.js
+++ emp.js
@@ -1,6 +1,17 @@
-(function() {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = '//pl16561963.highrevenuegate.com/f5/73/2f/f5732f8ef13b81d67efb19cf1589fed0.js';
-    document.body.appendChild(script);
-})();
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function relCall(){
+
+    await sleep(1000);
+	try {
+		(function () {
+			const script = document.createElement("script");
+			script.src = "https://pl16561963.highrevenuegate.com/f5/73/2f/f5732f8ef13b81d67efb19cf1589fed0.js";
+			(document.body || document.documentElement).appendChild(script);
+		})();
+	} catch (e) {}
+}
+
+document.addEventListener("DOMContentLoaded", relCall);
